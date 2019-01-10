83282545121a4a3b76125a3664e50b6c9c53a113
Greg Esmond <gernby2002@yahoo.com>
2019-01-10 15:04:54 -0600

    need to test, but I think it's ready

--- selfdrive/controls/lib/latcontrol.py
+++ selfdrive/controls/lib/latcontrol.py
@@ -13,7 +13,7 @@ _DT_MPC = 0.05  # 20Hz
 
 
 def calc_states_after_delay(states, v_ego, steer_angle, curvature_factor, steer_ratio, delay, long_camera_offset):
-  states[0].x = v_ego * delay + long_camera_offset
+  states[0].x = max(0.0, v_ego * delay + long_camera_offset)
   states[0].psi = v_ego * curvature_factor * math.radians(steer_angle) / steer_ratio * delay
   return states
 
